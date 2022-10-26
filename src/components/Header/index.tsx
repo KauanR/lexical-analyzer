203ef7545f5bd28f16685ccde913bcb749a4610b
KauanR <kau.matosr@gmail.com>
2022-10-26 19:43:49 -0300

    Feat: Header component implementation

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import './styles.scss'
+
+export const Header = () => {
+
+    const githubRedirect = () => {
+        window.location.href = 'https://github.com/KauanR/lexical-analyzer'
+    }
+
+    return (
+        <AppBar className='header'>
+            <Toolbar>
+                <div className="spacer"></div>
+                <Typography variant='h6'>
+                    Analisador Léxico
+                </Typography>
+                <IconButton onClick={githubRedirect}>
+                    <GitHubIcon/>
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    )
+}
