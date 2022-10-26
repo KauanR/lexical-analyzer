9e04a83a243452c0b2ffdcaf5f3b3ea2aea68c1c
KauanR <kau.matosr@gmail.com>
2022-10-26 17:53:44 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import './App.scss'
+
+function App() {
+    return (
+        <Typography variant='h1'>
+            Lexical Analyzer
+        </Typography>
+    )
+}
+
+export default App
